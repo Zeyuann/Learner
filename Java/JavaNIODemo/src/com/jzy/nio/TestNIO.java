746eb709ccd176778d35ace1329b44f18ccb6b4a
zeyuann <jzyzhuce@163.com>
2015-04-10 15:34:01 +0800

    写了关于cpp文件夹的说明 ------------------------- Write description(README.md) about cpp folder.

--- Java/JavaNIODemo/src/com/jzy/nio/TestNIO.java
+++
@@ -1,7 +0,0 @@
-package com.jzy.nio;
-
-public class TestNIO {
-	public static void main(String[] args) {
-		System.out.println("haha");
-	}
-}
---
+++ cpp/main.cpp
@@ -0,0 +1,7 @@
+
+
+//include各个子文件夹下的main.h,就可以跑那个文件
+
+int main(int argc, char **argv) {
+
+}
